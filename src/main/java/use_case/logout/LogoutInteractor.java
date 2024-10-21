aaee04d10d3d0eeb9964877a85690eab10490189
ethanliu24 <liu.ethan2005@gmail.com>
2024-10-21 12:01:05 -0400

    added field implemented output usecase

--- src/main/java/use_case/logout/LogoutInteractor.java
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -4,8 +4,8 @@ package use_case.logout;
  * The Logout Interactor.
  */
 public class LogoutInteractor implements LogoutInputBoundary {
-    private LogoutUserDataAccessInterface userDataAccessObject;
-    private LogoutOutputBoundary logoutPresenter;
+    private final LogoutUserDataAccessInterface userDataAccessObject;
+    private final LogoutOutputBoundary logoutPresenter;
 
     public LogoutInteractor(LogoutUserDataAccessInterface userDataAccessInterface,
                             LogoutOutputBoundary logoutOutputBoundary) {
@@ -19,7 +19,7 @@ public class LogoutInteractor implements LogoutInputBoundary {
         // * set the username to null in the DAO
         // * instantiate the `LogoutOutputData`, which needs to contain the username.
         // * tell the presenter to prepare a success view.
-        final String username = logoutInputData.getCurrentUsername();
+        final String username = logoutInputData.getUsername();
         userDataAccessObject.setCurrentUsername(null);
         LogoutOutputData logoutOuput = new LogoutOutputData(username, false);
         this.logoutPresenter.prepareSuccessView(logoutOuput);
