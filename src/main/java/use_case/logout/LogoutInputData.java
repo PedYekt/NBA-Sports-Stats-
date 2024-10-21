99b273898b0ff4c5e71cf6e61f1db60a7c4ab3de
207moment <michelle.bakman@mail.utoronto.ca>
2024-10-21 15:58:19 -0400

    Finished TODOs for logout under InputData, Interactor, and OutputData. Also the commit includes the .addLogoutUseCase() part of main

--- src/main/java/use_case/logout/LogoutInputData.java
+++ src/main/java/use_case/logout/LogoutInputData.java
@@ -5,8 +5,13 @@ package use_case.logout;
  */
 public class LogoutInputData {
 
+    private final String username;
+
     public LogoutInputData(String username) {
-        // TODO: save the current username in an instance variable and add a getter.
+        this.username = username;
+        // * save the current username in an instance variable and add a getter.
     }
+
+    public String getUsername() { return username;}
 
 }
--- src/main/java/use_case/logout/LogoutInteractor.java
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -4,18 +4,26 @@ package use_case.logout;
  * The Logout Interactor.
  */
 public class LogoutInteractor implements LogoutInputBoundary {
-    private LogoutUserDataAccessInterface userDataAccessObject;
-    private LogoutOutputBoundary logoutPresenter;
+    private final LogoutUserDataAccessInterface userDataAccessObject;
+    private final LogoutOutputBoundary logoutPresenter;
 
     public LogoutInteractor(LogoutUserDataAccessInterface userDataAccessInterface,
                             LogoutOutputBoundary logoutOutputBoundary) {
-        // TODO: save the DAO and Presenter in the instance variables.
+        this.userDataAccessObject = userDataAccessInterface;
+        this.logoutPresenter = logoutOutputBoundary;
+        // * save the DAO and Presenter in the instance variables.
         // Which parameter is the DAO and which is the presenter?
     }
 
     @Override
     public void execute(LogoutInputData logoutInputData) {
-        // TODO: implement the logic of the Logout Use Case (depends on the LogoutInputData.java TODO)
+        String username = logoutInputData.getUsername();
+
+        userDataAccessObject.setCurrentUsername(null);
+
+        LogoutOutputData logoutOutputData = new LogoutOutputData(username, false);
+        logoutPresenter.prepareSuccessView(logoutOutputData);
+        // * implement the logic of the Logout Use Case (depends on the LogoutInputData.java task)
         // * get the username out of the input data,
         // * set the username to null in the DAO
         // * instantiate the `LogoutOutputData`, which needs to contain the username.
