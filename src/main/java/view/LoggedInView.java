0f4cf1960c4596eba94572208943589a4bddb253
Alisa <157826128+alisa-isk@users.noreply.github.com>
2024-10-21 14:25:36 -0400

    apparently i changed something?

--- src/main/java/view/LoggedInView.java
+++ src/main/java/view/LoggedInView.java
@@ -16,6 +16,7 @@ import javax.swing.event.DocumentListener;
 import interface_adapter.change_password.ChangePasswordController;
 import interface_adapter.change_password.LoggedInState;
 import interface_adapter.change_password.LoggedInViewModel;
+import interface_adapter.login.LoginController;
 import interface_adapter.logout.LogoutController;
 
 /**
