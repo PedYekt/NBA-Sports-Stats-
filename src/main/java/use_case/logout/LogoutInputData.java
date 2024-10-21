cedf97b2c5b36217d05289c85a69b2c0f76050a8
Xing Xu <sincer.xu@mail.utoronto.ca>
2024-10-21 16:12:00 -0400

    Update LogoutInputData.java

--- src/main/java/use_case/logout/LogoutInputData.java
+++ src/main/java/use_case/logout/LogoutInputData.java
@@ -6,8 +6,6 @@ package use_case.logout;
 public class LogoutInputData {
     private final String username;
 
-    private final String username;
-
     public LogoutInputData(String username) {
         // TODO: save the current username in an instance variable and add a getter.
         this.username = username;
