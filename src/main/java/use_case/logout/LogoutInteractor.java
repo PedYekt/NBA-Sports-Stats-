12bb1007a1632ab2a2106468167a7a67dbc4f882
Kinley-Padon <145949013+Kinley-Padon@users.noreply.github.com>
2024-10-21 12:05:03 -0400

    "Third part of the TODOs."

--- src/main/java/use_case/logout/LogoutInteractor.java
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -10,6 +10,8 @@ public class LogoutInteractor implements LogoutInputBoundary {
     public LogoutInteractor(LogoutUserDataAccessInterface userDataAccessInterface,
                             LogoutOutputBoundary logoutOutputBoundary) {
         // TODO: save the DAO and Presenter in the instance variables.
+        this.userDataAccessObject = userDataAccessInterface;
+        this.logoutPresenter = logoutOutputBoundary;
         // Which parameter is the DAO and which is the presenter?
     }
 
@@ -20,6 +22,12 @@ public class LogoutInteractor implements LogoutInputBoundary {
         // * set the username to null in the DAO
         // * instantiate the `LogoutOutputData`, which needs to contain the username.
         // * tell the presenter to prepare a success view.
+        final String username = logoutInputData.getUsername();
+        userDataAccessObject.setCurrentUsername(username);
+        final LogoutOutputData logoutOutputData = new LogoutOutputData(username, false);
+
+        logoutPresenter.prepareSuccessView(logoutOutputData);
+
     }
 }
 
