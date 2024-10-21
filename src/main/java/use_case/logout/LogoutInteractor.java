45ead3860eb78c07537ac524ade680ee6ed7b6af
lxyhan <hanlyu2005@gmail.com>
2024-10-21 13:46:22 -0400

    completed logout interactor constructor

--- src/main/java/use_case/logout/LogoutInteractor.java
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -9,8 +9,8 @@ public class LogoutInteractor implements LogoutInputBoundary {
 
     public LogoutInteractor(LogoutUserDataAccessInterface userDataAccessInterface,
                             LogoutOutputBoundary logoutOutputBoundary) {
-        // TODO: save the DAO and Presenter in the instance variables.
-        // Which parameter is the DAO and which is the presenter?
+        userDataAccessObject = userDataAccessInterface;
+        logoutPresenter = logoutOutputBoundary;
     }
 
     @Override
