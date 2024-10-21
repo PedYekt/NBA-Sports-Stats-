da695c8e9aceb79f1219ff0509e4b4249e311aef
feiwun <karen26wong@gmail.com>
2024-10-21 11:45:14 -0400

    LoggedInView.java TODO task done

--- src/main/java/view/LoggedInView.java
+++ src/main/java/view/LoggedInView.java
@@ -16,6 +16,7 @@ import javax.swing.event.DocumentListener;
 import interface_adapter.change_password.ChangePasswordController;
 import interface_adapter.change_password.LoggedInState;
 import interface_adapter.change_password.LoggedInViewModel;
+import interface_adapter.login.LoginState;
 import interface_adapter.logout.LogoutController;
 
 /**
@@ -102,7 +103,10 @@ public class LoggedInView extends JPanel implements PropertyChangeListener {
                     if (evt.getSource().equals(logOut)) {
                         // TODO: execute the logout use case through the Controller
                         // 1. get the state out of the loggedInViewModel. It contains the username.
+                        final LoggedInState currentState = loggedInViewModel.getState();
+
                         // 2. Execute the logout Controller.
+                        logoutController.execute(currentState.getUsername());
                     }
                 }
         );
