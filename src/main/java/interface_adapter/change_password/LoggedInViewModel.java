b671662911d4871c5040ed72bb0a6e05cb5bac69
YifanHan07 <yifan.han@mail.utoronto.ca>
2024-10-23 17:38:14 -0400

    finish logout presenter

--- src/main/java/interface_adapter/change_password/LoggedInViewModel.java
+++ src/main/java/interface_adapter/change_password/LoggedInViewModel.java
@@ -12,4 +12,19 @@ public class LoggedInViewModel extends ViewModel<LoggedInState> {
         setState(new LoggedInState());
     }
 
+    public LoggedInState getLoggedInState() {
+        return this.getState();
+    }
+
+    /**
+     * Sets the current state of the logged-in user.
+     * This method updates the current LoggedInState to the provided state and triggers
+     * a property change event to notify the view that the state has been updated.
+     *
+     * @param loggedInState The new state representing the logged-in user.
+     */
+    public void setLoggedInState(LoggedInState loggedInState) {
+        this.setState(loggedInState);
+        this.firePropertyChanged();
+    }
 }
