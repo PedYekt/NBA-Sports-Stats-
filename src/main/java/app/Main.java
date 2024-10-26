c012f6e1854b393a435a03c01a146f3c8fac64ae
jiayunkang <jaren.kang@mail.utoronto.ca>
2024-10-26 15:01:47 -0400

    try

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -12,7 +12,7 @@ public class Main {
      */
     public static void main(String[] args) {
         final AppBuilder appBuilder = new AppBuilder();
-        // TODO[done]: add the Logout Use Case to the app using the appBuilder
+        // TODO[done!]: add the Logout Use Case to the app using the appBuilder
         final JFrame application = appBuilder
                                             .addLoginView()
                                             .addSignupView()
