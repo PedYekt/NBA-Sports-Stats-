dd68a209c43c734ac96bf1e4ccf462a78c2e764c
Ped Y <pedram.yekta12@gmail.com>
2024-11-04 10:54:51 -0500

    example

--- src/main/java/data_access/PlayerDataAccessObject.java
+++ src/main/java/data_access/PlayerDataAccessObject.java
@@ -1,2 +1,37 @@
-package data_access;public class PlayerDataAccessObject {
+package data_access;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * PlayerDataAccessObject class to handle player data access.
+ */
+public class PlayerDataAccessObject {
+
+    /**
+     * Main method to execute the data access.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        final OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        final Request request = new Request.Builder()
+                .url("http://b8c40s8.143.198.70.30.sslip.io/api/PlayerDataTotals/name/LeBron James")
+                .get()
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                System.out.println("Response Code: " + response.code());
+                System.out.println("Response Body: " + response.body().string());
+            } else {
+                System.out.println("Request failed with code: " + response.code());
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
