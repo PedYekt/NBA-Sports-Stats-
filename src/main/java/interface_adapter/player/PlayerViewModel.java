12b58e1649be4c9aea6a4efcf4a913aec15799ef
Emanuel <e.grillone@mail.utoronto.ca>
2024-11-18 09:11:22 -0500

    Got basic player view up and running - Emanuel

--- src/main/java/interface_adapter/player/PlayerViewModel.java
+++ src/main/java/interface_adapter/player/PlayerViewModel.java
@@ -1,4 +1,6 @@
 package interface_adapter.player;
 
 public class PlayerViewModel {
+
+    public static final String TITLE_LABEL = "LIST OF PLAYERS ";
 }
--- src/main/java/view/ViewManager.java
+++ src/main/java/view/ViewManager.java
@@ -1,4 +1,29 @@
 package view;
 
-public class ViewManager {
+import java.awt.CardLayout;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.JPanel;
+
+/**
+ * The View Manager for the program. It listens for property change events
+ * in the ViewManagerModel and updates which View should be visible.
+ */
+public class ViewManager implements PropertyChangeListener {
+    private final CardLayout cardLayout;
+    private final JPanel views;
+
+    public ViewManager(JPanel views, CardLayout cardLayout) {
+        this.views = views;
+        this.cardLayout = cardLayout;
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        if (evt.getPropertyName().equals("state")) {
+            final String viewModelName = (String) evt.getNewValue();
+            cardLayout.show(views, viewModelName);
+        }
+    }
 }
